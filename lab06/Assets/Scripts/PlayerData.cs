7bf9c73d3810138ede23fac743a3e5eff732b62e
169323 <169323@student.uwm.edu.pl>
2025-11-24 09:58:21 +0100

    lab07

---
+++ lab06/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPlayerData", menuName = "Player Data")]
+public class PlayerData : ScriptableObject
+{
+    public string playerName;
+    public int playerScore;
+    public int playerHealth;
+
+    // Dodaj dodatkowe pola w zale¿noœci od potrzeb
+}
